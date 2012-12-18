d885c9dcca441a805d8c54bb67909d19f5a8f3fc
Kuogee Hsieh <khsieh@codeaurora.org>
2012-12-18 09:40:25 -0800

    msm_fb: display: add panel's late_init
    Some panels need to be initialized after panel is on.
    Add late_init to support this feature.
    
    CRs-fixed: 433011
    Change-Id: I73c623d1fe52363f070af79ce4bf7774cd84689e
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>

--- drivers/video/msm/mipi_dsi.c
+++ drivers/video/msm/mipi_dsi.c
@@ -358,6 +358,11 @@ err:
 	return ret;
 }
 
+static int mipi_dsi_late_init(struct platform_device *pdev)
+{
+	return panel_next_late_init(pdev);
+}
+
 
 static int mipi_dsi_resource_initialized;
 
@@ -507,6 +512,7 @@ static int mipi_dsi_probe(struct platform_device *pdev)
 	pdata = mdp_dev->dev.platform_data;
 	pdata->on = mipi_dsi_on;
 	pdata->off = mipi_dsi_off;
+	pdata->late_init = mipi_dsi_late_init;
 	pdata->next = pdev;
 
 	/*
