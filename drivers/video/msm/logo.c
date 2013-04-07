987e7adb528bf115c264cd926d365f1616639160
Alin Jerpelea <jerpelea@gmail.com>
2013-04-07 09:08:14 +0300

    tamsui: logo cleanup
    Change-Id: I5083a77853c7ed9b9443372a5d2ed9523b90404d

--- drivers/video/msm/logo.c
+++ drivers/video/msm/logo.c
@@ -46,13 +46,6 @@
 #define rgb32_b(rle) (((rle & 0x001f) << 3))  
 
 #define rgb32(rle) (rgb32_r(rle) << 16 | rgb32_g(rle) << 8 | rgb32_b(rle) << 0)   
-static void memset32(void *_ptr, uint32_t val, unsigned count)
- {
-       uint32_t *ptr = _ptr;
-       count >>= 2;
-         while (count--)
-                 *ptr++ = val;
- }
 
 #if defined(CONFIG_FB_MSM_DEFAULT_DEPTH_RGB565)
 static void memset16(void *_ptr, unsigned short val, unsigned count)
@@ -62,76 +55,6 @@ static void memset16(void *_ptr, unsigned short val, unsigned count)
 	while (count--)
 		*ptr++ = val;
 }
-#endif
-/* 565RLE image format: [count(2 bytes), rle(2 bytes)] */
-int load_565rle_image(char *filename, bool bf_supported)
-{
-	struct fb_info *info;
-	int fd, count, err = 0;
-	unsigned max;
-	unsigned short *data, *ptr ;
-	uint32_t *bits;
-	unsigned int out;
-
-	info = registered_fb[0];
-	if (!info) {
-		printk(KERN_WARNING "%s: Can not access framebuffer\n",
-			__func__);
-		return -ENODEV;
-	}
-
-	fd = sys_open(filename, O_RDONLY, 0);
-	if (fd < 0) {
-		printk(KERN_WARNING "%s: Can not open %s\n",
-			__func__, filename);
-		return -ENOENT;
-	}
-	count = sys_lseek(fd, (off_t)0, 2);
-	if (count <= 0) {
-		err = -EIO;
-		goto err_logo_close_file;
-	}
-	sys_lseek(fd, (off_t)0, 0);
-	data = kmalloc(count, GFP_KERNEL);
-	if (!data) {
-		printk(KERN_WARNING "%s: Can not alloc data\n", __func__);
-		err = -ENOMEM;
-		goto err_logo_close_file;
-	}
-	if (sys_read(fd, (char *)data, count) != count) {
-		err = -EIO;
-		goto err_logo_free_data;
-	}
-
-	max = fb_width(info) * fb_height(info);
-	ptr = data;
-	if (bf_supported && (info->node == 1 || info->node == 2)) {
-		err = -EPERM;
-		pr_err("%s:%d no info->creen_base on fb%d!\n",
-		       __func__, __LINE__, info->node);
-		goto err_logo_free_data;
-	}
-	bits = (uint32_t*)(info->screen_base);
-	while (count > 3) {
-		unsigned n = ptr[0];
-		if (n > max)
-			break;
-		out = rgb32(ptr[1]);  
-
-		memset32(bits, out, n << 2);
-		bits += n;
-		max -= n;
-		ptr += 2;
-		count -= 4;
-	}
-
-err_logo_free_data:
-	kfree(data);
-err_logo_close_file:
-	sys_close(fd);
-	return err;
-}
-EXPORT_SYMBOL(load_565rle_image);
 
 int fih_load_565rle_image(char *filename)
 {
@@ -209,17 +132,24 @@ error2:
 	return err;
 }
 
-int fih_dump_framebuffer(char *filename)
+#else
+static void memset32(void *_ptr, uint32_t val, unsigned count)
+ {
+       uint32_t *ptr = _ptr;
+       count >>= 2;
+         while (count--)
+                 *ptr++ = val;
+ }
+
+/* 565RLE image format: [count(2 bytes), rle(2 bytes)] */
+int load_565rle_image(char *filename, bool bf_supported)
 {
-	struct fb_info *info  = NULL;
-	struct file    *filp  = NULL;
-	unsigned char  *bits = NULL;
-	int count = 0, err = 0;
-
-	mm_segment_t old_fs = get_fs();
-	set_fs (get_ds());
-
-	printk(KERN_INFO "[DISPLAY] %s\n", __func__);
+	struct fb_info *info;
+	int fd, count, err = 0;
+	unsigned max;
+	unsigned short *data, *ptr ;
+	uint32_t *bits;
+	unsigned int out;
 
 	info = registered_fb[0];
 	if (!info) {
@@ -228,27 +158,57 @@ int fih_dump_framebuffer(char *filename)
 		return -ENODEV;
 	}
 
-	bits = (unsigned char *)(info->screen_base);
-	filp = filp_open(filename, O_RDWR | O_CREAT, 0644);
-	if (IS_ERR(filp)) {
+	fd = sys_open(filename, O_RDONLY, 0);
+	if (fd < 0) {
 		printk(KERN_WARNING "%s: Can not open %s\n",
 			__func__, filename);
-		err = -ENOENT;
-		goto error1;
+		return -ENOENT;
 	}
-#ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
-	count = 3*fb_size(info);
-#else
-	count = 2*fb_size(info);
-#endif
-
-	if (filp->f_op->write(filp, bits, count, &filp->f_pos) < 0) {
-		printk(KERN_WARNING "%s: write file error?\n", __func__);
+	count = sys_lseek(fd, (off_t)0, 2);
+	if (count <= 0) {
 		err = -EIO;
+		goto err_logo_close_file;
+	}
+	sys_lseek(fd, (off_t)0, 0);
+	data = kmalloc(count, GFP_KERNEL);
+	if (!data) {
+		printk(KERN_WARNING "%s: Can not alloc data\n", __func__);
+		err = -ENOMEM;
+		goto err_logo_close_file;
+	}
+	if (sys_read(fd, (char *)data, count) != count) {
+		err = -EIO;
+		goto err_logo_free_data;
 	}
 
-	filp_close(filp, NULL);
-error1:
-	set_fs(old_fs);
+	max = fb_width(info) * fb_height(info);
+	ptr = data;
+	if (bf_supported && (info->node == 1 || info->node == 2)) {
+		err = -EPERM;
+		pr_err("%s:%d no info->creen_base on fb%d!\n",
+		       __func__, __LINE__, info->node);
+		goto err_logo_free_data;
+	}
+	bits = (uint32_t*)(info->screen_base);
+	while (count > 3) {
+		unsigned n = ptr[0];
+		if (n > max)
+			break;
+		out = rgb32(ptr[1]);  
+
+		memset32(bits, out, n << 2);
+		bits += n;
+		max -= n;
+		ptr += 2;
+		count -= 4;
+	}
+
+err_logo_free_data:
+	kfree(data);
+err_logo_close_file:
+	sys_close(fd);
 	return err;
 }
+EXPORT_SYMBOL(load_565rle_image);
+
+#endif
